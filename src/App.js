f0fb2e5d284f4247a8e37665507d40b2b9c6f285
Chen <164990359@qq.com>
2017-11-30 17:01:43 +0800

    引入antd，配置按需加载antd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Button } from 'antd';
 
 class App extends Component {
    
@@ -16,6 +17,7 @@ class App extends Component {
                     To get started, edit <code>src/App.js</code> and save to
                     reload.
                 </p>
+                <p> <Button type="primary">Primary</Button></p>
             </div>
         );
     }
